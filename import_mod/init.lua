90d49badd51ba3b711a5733ddb516205e5194c2b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-10-21 14:37:58 +0200

    import mod

--- import_mod/init.lua
+++ import_mod/init.lua
@@ -17,3 +17,13 @@ if config.enable_spawnpoint then
   -- initialize player spawn point
   spawn(manifest);
 end
+
+if minetest.get_modpath("modgen") then
+  -- modgen available
+
+  -- set current map export limits to modgen.pos1/pos2
+  -- this allows re-exporting a map 1:1 with just the /export command
+  local origin = { x=0, y=0, z=0 }
+  modgen.set_pos(1, "singleplayer", origin)
+  modgen.set_pos(2, "singleplayer", vector.multiply(manifest.size_mapblocks), 16)
+end
