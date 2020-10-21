f7c1036b74d6d7f0e8512770a87aeb4ba45be631
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-10-21 20:07:05 +0200

    defer pos setting

--- import_mod/init.lua
+++ import_mod/init.lua
@@ -23,7 +23,9 @@ if minetest.get_modpath("modgen") then
 
   -- set current map export limits to modgen.pos1/pos2
   -- this allows re-exporting a map 1:1 with just the /export command
-  local origin = { x=0, y=0, z=0 }
-  modgen.set_pos(1, "singleplayer", origin)
-  modgen.set_pos(2, "singleplayer", vector.multiply(manifest.size_mapblocks, 16))
+  minetest.after(0, function()
+    local origin = { x=0, y=0, z=0 }
+    modgen.set_pos(1, "singleplayer", origin)
+    modgen.set_pos(2, "singleplayer", vector.multiply(manifest.size_mapblocks, 16))
+  end)
 end
