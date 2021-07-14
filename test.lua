dd0df2f1153ee1e4dae6d4284be20c1f46311a75
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-07-14 15:27:19 +0200

    add integration test skeleton

---
+++ test.lua
@@ -0,0 +1,6 @@
+if minetest.settings:get_bool("enable_integration_test") then
+    minetest.register_on_mods_loaded(function()
+        -- just exit gracefully
+        minetest.request_shutdown("success")
+    end)
+end
