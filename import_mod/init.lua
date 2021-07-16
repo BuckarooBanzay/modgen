c03b998a9c722560426235e66993f1b46bcaaa49
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-07-16 21:05:14 +0200

    add world uid safety checks

--- import_mod/init.lua
+++ import_mod/init.lua
@@ -1,6 +1,13 @@
+--- Modgen import mod
+-- writes the mapblocks back to the world
+-- dependency- and global-free
+
 -- mod name and path
 local modname = minetest.get_current_modname()
 local MP = minetest.get_modpath(modname)
+
+-- storage
+local storage = minetest.get_mod_storage()
 
 -- local functions/helpers
 local mapgen = dofile(MP .. "/mapgen.lua")
@@ -8,6 +15,14 @@ local read_manifest = dofile(MP .. "/read_manifest.lua")
 local nodename_check = dofile(MP .. "/nodename_check.lua")
 
 local manifest = read_manifest()
+local world_uid = storage:get_string("uid")
+if world_uid ~= "" and world_uid ~= manifest.uid then
+  -- abort if the uids don't match, something fishy might be going on
+  error("modgen uids don't match, aborting for your safety!")
+end
+
+-- write modgen uid to world-storage
+storage:set_string("uid", manifest.uid)
 
 -- check if the nodes are available in the current world
 minetest.register_on_mods_loaded(function()
