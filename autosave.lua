0056f4c92b4ff8e1b3adf35c55ce5ca42214ee3c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-08-29 19:58:56 +0200

    autosave fixes

--- autosave.lua
+++ autosave.lua
@@ -1,22 +1,23 @@
 
 
--- list of mapblocks marked for export
-local mapblocks = {}
+-- list of chunks marked for export
+local chunks = {}
 
 -- autosave worker function
 local function worker()
     local count = 0
-    for hash in pairs(mapblocks) do
+    for hash in pairs(chunks) do
         count = count + 1
-        local mapblock_pos = minetest.get_position_from_hash(hash)
-        local pos = modgen.get_mapblock_bounds_from_mapblock(mapblock_pos)
-        modgen.export("singleplayer", pos, pos, true, false)
+        local chunk_pos = minetest.get_position_from_hash(hash)
+        local mapblock_pos = modgen.get_mapblock_bounds_from_chunk(chunk_pos)
+        local min = modgen.get_mapblock_bounds_from_mapblock(mapblock_pos)
+        modgen.export("singleplayer", min, min, true, false)
     end
 
     if count > 0 then
-        minetest.chat_send_all("Dispatched " .. count .. " mapblocks to export")
+        minetest.chat_send_all("Dispatched " .. count .. " chunk(s) to export")
     end
-    mapblocks = {}
+    chunks = {}
     minetest.after(2, worker)
 end
 
@@ -29,14 +30,14 @@ local function deferred_export(pos1, pos2)
 
     pos1, pos2 = modgen.sort_pos(pos1, pos2)
 
-    local mapblock_pos1 = modgen.get_mapblock(pos1)
-    local mapblock_pos2 = modgen.get_mapblock(pos2)
-    for x=mapblock_pos1.x,mapblock_pos2.x do
-        for y=mapblock_pos1.y,mapblock_pos2.y do
-            for z=mapblock_pos1.z,mapblock_pos2.z do
-                local mapblock_pos = {x=x, y=y, z=z}
-                local hash = minetest.hash_node_position(mapblock_pos)
-                mapblocks[hash] = true
+    local chunk_pos1 = modgen.get_chunkpos(pos1)
+    local chunk_pos2 = modgen.get_chunkpos(pos2)
+    for x=chunk_pos1.x,chunk_pos2.x do
+        for y=chunk_pos1.y,chunk_pos2.y do
+            for z=chunk_pos1.z,chunk_pos2.z do
+                local chunk_pos = {x=x, y=y, z=z}
+                local hash = minetest.hash_node_position(chunk_pos)
+                chunks[hash] = true
             end
         end
     end
